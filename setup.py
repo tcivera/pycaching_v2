ec57885c6b29c38b5021c45fe011f074bf2359a2
tcivera <tcivera@users.noreply.github.com>
2021-08-11 17:13:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='pycaching_v2',
+      version='1.0.0',
+      description="My own pycaching library based on the officila one",
+      author='Tamara Civera',
+      author_email=',
+      url='',
+      license='GPL3',
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+        "Development Status :: 0 - Beta",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Entertaiment",
+      ],
+      packages=['pycaching_v2'],
+      package_dir={'pycaching_v2': ''}
+      )
